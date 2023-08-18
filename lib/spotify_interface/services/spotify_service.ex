d7396bdc56c40df97ceb3e9d5d9ec1249ea8640e
ASASauqui <ASASauqui@protonmail.com>
2023-08-18 14:07:59 -0600

    API response fixed

--- lib/spotify_interface/services/spotify_service.ex
+++ lib/spotify_interface/services/spotify_service.ex
@@ -12,79 +12,82 @@ defmodule SpotifyInterface.Services.SpotifyService do
     encoded_search = URI.encode_www_form(search)
 
     response = HTTPoison.get!("#{@base_url}#{endpoint}?q=#{encoded_search}&type=album%2Cartist%2Ctrack&market=ES", headers, [])
-    |> Map.get(:body)
-    |> Jason.decode!()
 
-    if response["error"] do
-      {:error, response["error"]}
-    else
-      artists = Enum.map(response["artists"]["items"], fn artist ->
-        %Artist{
-          external_urls: artist["external_urls"],
-          followers_href: artist["followers"]["href"],
-          total_followers: artist["followers"]["total"],
-          genres: artist["genres"],
-          genres_href: artist["href"],
-          id: artist["id"],
-          images: artist["images"],
-          name: artist["name"],
-          popularity: artist["popularity"],
-          type: artist["type"],
-          uri: artist["uri"]
+    case response.status_code do
+      200 ->
+        decoded_response = response
+          |> Map.get(:body)
+          |> Jason.decode!()
+
+        artists = Enum.map(decoded_response["artists"]["items"], fn artist ->
+          %Artist{
+            external_urls: artist["external_urls"],
+            followers_href: artist["followers"]["href"],
+            total_followers: artist["followers"]["total"],
+            genres: artist["genres"],
+            genres_href: artist["href"],
+            id: artist["id"],
+            images: artist["images"],
+            name: artist["name"],
+            popularity: artist["popularity"],
+            type: artist["type"],
+            uri: artist["uri"]
+          }
+        end)
+
+        albums = Enum.map(decoded_response["albums"]["items"], fn album ->
+          %Album{
+            album_type: album["album_type"],
+            total_tracks: album["total_tracks"],
+            available_markets: album["available_markets"],
+            external_urls: album["external_urls"],
+            href: album["href"],
+            id: album["id"],
+            images: album["images"],
+            name: album["name"],
+            release_date: album["release_date"],
+            release_date_precision: album["release_date_precision"],
+            restrictions: album["restrictions"],
+            type: album["type"],
+            uri: album["uri"],
+            genres: album["genres"],
+            label: album["label"],
+            popularity: album["popularity"]
+          }
+        end)
+
+        tracks = Enum.map(decoded_response["tracks"]["items"], fn track ->
+          %Track{
+            id: track["id"],
+            available_markets: track["available_markets"],
+            disc_number: track["disc_number"],
+            duration_ms: track["duration_ms"],
+            explicit: track["explicit"],
+            external_ids: track["external_ids"],
+            external_urls: track["external_urls"],
+            href: track["href"],
+            is_playable: track["is_playable"],
+            linked_from: track["linked_from"],
+            restrictions: track["restrictions"],
+            name: track["name"],
+            popularity: track["popularity"],
+            preview_url: track["preview_url"],
+            track_number: track["track_number"],
+            type: track["type"],
+            uri: track["uri"],
+            is_local: track["is_local"],
+            artists: track["artists"],
+            album: track["album"]
+          }
+        end)
+
+        %{
+          artists: artists,
+          albums: albums,
+          tracks: tracks
         }
-      end)
-
-      albums = Enum.map(response["albums"]["items"], fn album ->
-        %Album{
-          album_type: album["album_type"],
-          total_tracks: album["total_tracks"],
-          available_markets: album["available_markets"],
-          external_urls: album["external_urls"],
-          href: album["href"],
-          id: album["id"],
-          images: album["images"],
-          name: album["name"],
-          release_date: album["release_date"],
-          release_date_precision: album["release_date_precision"],
-          restrictions: album["restrictions"],
-          type: album["type"],
-          uri: album["uri"],
-          genres: album["genres"],
-          label: album["label"],
-          popularity: album["popularity"]
-        }
-      end)
-
-      tracks = Enum.map(response["tracks"]["items"], fn track ->
-        %Track{
-          id: track["id"],
-          available_markets: track["available_markets"],
-          disc_number: track["disc_number"],
-          duration_ms: track["duration_ms"],
-          explicit: track["explicit"],
-          external_ids: track["external_ids"],
-          external_urls: track["external_urls"],
-          href: track["href"],
-          is_playable: track["is_playable"],
-          linked_from: track["linked_from"],
-          restrictions: track["restrictions"],
-          name: track["name"],
-          popularity: track["popularity"],
-          preview_url: track["preview_url"],
-          track_number: track["track_number"],
-          type: track["type"],
-          uri: track["uri"],
-          is_local: track["is_local"],
-          artists: track["artists"],
-          album: track["album"]
-        }
-      end)
-
-      %{
-        artists: artists,
-        albums: albums,
-        tracks: tracks
-      }
+       _ ->
+        {:error, %{"status" => response.status_code, "message" => response.body}}
     end
   end
 
@@ -93,30 +96,33 @@ defmodule SpotifyInterface.Services.SpotifyService do
     headers = [{"Authorization", "Bearer #{access_token}"}]
 
     response = HTTPoison.get!("#{@base_url}#{endpoint}/#{id}?market=ES", headers, [])
-    |> Map.get(:body)
-    |> Jason.decode!()
 
-    if response["error"] do
-      {:error, response["error"]}
-    else
-      %Album{
-        album_type: response["album_type"],
-        total_tracks: response["total_tracks"],
-        available_markets: response["available_markets"],
-        external_urls: response["external_urls"],
-        href: response["href"],
-        id: response["id"],
-        images: response["images"],
-        name: response["name"],
-        release_date: response["release_date"],
-        release_date_precision: response["release_date_precision"],
-        restrictions: response["restrictions"],
-        type: response["type"],
-        uri: response["uri"],
-        genres: response["genres"],
-        label: response["label"],
-        popularity: response["popularity"]
-      }
+    case response.status_code do
+      200 ->
+        decoded_response = response
+          |> Map.get(:body)
+          |> Jason.decode!()
+
+        %Album{
+          album_type: decoded_response["album_type"],
+          total_tracks: decoded_response["total_tracks"],
+          available_markets: decoded_response["available_markets"],
+          external_urls: decoded_response["external_urls"],
+          href: decoded_response["href"],
+          id: decoded_response["id"],
+          images: decoded_response["images"],
+          name: decoded_response["name"],
+          release_date: decoded_response["release_date"],
+          release_date_precision: decoded_response["release_date_precision"],
+          restrictions: decoded_response["restrictions"],
+          type: decoded_response["type"],
+          uri: decoded_response["uri"],
+          genres: decoded_response["genres"],
+          label: decoded_response["label"],
+          popularity: decoded_response["popularity"]
+        }
+      _ ->
+        {:error, %{"status" => response.status_code, "message" => response.body}}
     end
   end
 
@@ -125,36 +131,39 @@ defmodule SpotifyInterface.Services.SpotifyService do
     headers = [{"Authorization", "Bearer #{access_token}"}]
 
     response = HTTPoison.get!("#{@base_url}#{endpoint}/#{id}/tracks?market=ES", headers, [])
-    |> Map.get(:body)
-    |> Jason.decode!()
 
-    if response["error"] do
-      {:error, response["error"]}
-    else
-      Enum.map(response["items"], fn track ->
-        %Track{
-          id: track["id"],
-          available_markets: track["available_markets"],
-          disc_number: track["disc_number"],
-          duration_ms: track["duration_ms"],
-          explicit: track["explicit"],
-          external_ids: track["external_ids"],
-          external_urls: track["external_urls"],
-          href: track["href"],
-          is_playable: track["is_playable"],
-          linked_from: track["linked_from"],
-          restrictions: track["restrictions"],
-          name: track["name"],
-          popularity: track["popularity"],
-          preview_url: track["preview_url"],
-          track_number: track["track_number"],
-          type: track["type"],
-          uri: track["uri"],
-          is_local: track["is_local"],
-          artists: track["artists"],
-          album: track["album"]
-        }
-      end)
+    case response.status_code do
+      200 ->
+        decoded_response = response
+          |> Map.get(:body)
+          |> Jason.decode!()
+
+        Enum.map(decoded_response["items"], fn track ->
+          %Track{
+            id: track["id"],
+            available_markets: track["available_markets"],
+            disc_number: track["disc_number"],
+            duration_ms: track["duration_ms"],
+            explicit: track["explicit"],
+            external_ids: track["external_ids"],
+            external_urls: track["external_urls"],
+            href: track["href"],
+            is_playable: track["is_playable"],
+            linked_from: track["linked_from"],
+            restrictions: track["restrictions"],
+            name: track["name"],
+            popularity: track["popularity"],
+            preview_url: track["preview_url"],
+            track_number: track["track_number"],
+            type: track["type"],
+            uri: track["uri"],
+            is_local: track["is_local"],
+            artists: track["artists"],
+            album: track["album"]
+          }
+        end)
+      _ ->
+        {:error, %{"status" => response.status_code, "message" => response.body}}
     end
   end
 
@@ -163,25 +172,28 @@ defmodule SpotifyInterface.Services.SpotifyService do
     headers = [{"Authorization", "Bearer #{access_token}"}]
 
     response = HTTPoison.get!("#{@base_url}#{endpoint}/#{id}", headers, [])
-    |> Map.get(:body)
-    |> Jason.decode!()
 
-    if response["error"] do
-      {:error, response["error"]}
-    else
-      %Artist{
-        external_urls: response["external_urls"],
-        followers_href: response["followers"]["href"],
-        total_followers: response["followers"]["total"],
-        genres: response["genres"],
-        genres_href: response["href"],
-        id: response["id"],
-        images: response["images"],
-        name: response["name"],
-        popularity: response["popularity"],
-        type: response["type"],
-        uri: response["uri"]
-      }
+    case response.status_code do
+      200 ->
+        decoded_response = response
+          |> Map.get(:body)
+          |> Jason.decode!()
+
+        %Artist{
+          external_urls: decoded_response["external_urls"],
+          followers_href: decoded_response["followers"]["href"],
+          total_followers: decoded_response["followers"]["total"],
+          genres: decoded_response["genres"],
+          genres_href: decoded_response["href"],
+          id: decoded_response["id"],
+          images: decoded_response["images"],
+          name: decoded_response["name"],
+          popularity: decoded_response["popularity"],
+          type: decoded_response["type"],
+          uri: decoded_response["uri"]
+        }
+      _ ->
+        {:error, %{"status" => response.status_code, "message" => response.body}}
     end
   end
 
@@ -190,32 +202,35 @@ defmodule SpotifyInterface.Services.SpotifyService do
     headers = [{"Authorization", "Bearer #{access_token}"}]
 
     response = HTTPoison.get!("#{@base_url}#{endpoint}/#{id}/albums?market=ES", headers, [])
-    |> Map.get(:body)
-    |> Jason.decode!()
 
-    if response["error"] do
-      {:error, response["error"]}
-    else
-      Enum.map(response["items"], fn album ->
-        %Album{
-          album_type: album["album_type"],
-          total_tracks: album["total_tracks"],
-          available_markets: album["available_markets"],
-          external_urls: album["external_urls"],
-          href: album["href"],
-          id: album["id"],
-          images: album["images"],
-          name: album["name"],
-          release_date: album["release_date"],
-          release_date_precision: album["release_date_precision"],
-          restrictions: album["restrictions"],
-          type: album["type"],
-          uri: album["uri"],
-          genres: album["genres"],
-          label: album["label"],
-          popularity: album["popularity"]
-        }
-      end)
+    case response.status_code do
+      200 ->
+        decoded_response = response
+          |> Map.get(:body)
+          |> Jason.decode!()
+
+        Enum.map(decoded_response["items"], fn album ->
+          %Album{
+            album_type: album["album_type"],
+            total_tracks: album["total_tracks"],
+            available_markets: album["available_markets"],
+            external_urls: album["external_urls"],
+            href: album["href"],
+            id: album["id"],
+            images: album["images"],
+            name: album["name"],
+            release_date: album["release_date"],
+            release_date_precision: album["release_date_precision"],
+            restrictions: album["restrictions"],
+            type: album["type"],
+            uri: album["uri"],
+            genres: album["genres"],
+            label: album["label"],
+            popularity: album["popularity"]
+          }
+        end)
+      _ ->
+        {:error, %{"status" => response.status_code, "message" => response.body}}
     end
   end
 
@@ -224,36 +239,39 @@ defmodule SpotifyInterface.Services.SpotifyService do
     headers = [{"Authorization", "Bearer #{access_token}"}]
 
     response = HTTPoison.get!("#{@base_url}#{endpoint}/#{id}/top-tracks?market=ES", headers, [])
-    |> Map.get(:body)
-    |> Jason.decode!()
 
-    if response["error"] do
-      {:error, response["error"]}
-    else
-      Enum.map(response["tracks"], fn track ->
-        %Track{
-          id: track["id"],
-          available_markets: track["available_markets"],
-          disc_number: track["disc_number"],
-          duration_ms: track["duration_ms"],
-          explicit: track["explicit"],
-          external_ids: track["external_ids"],
-          external_urls: track["external_urls"],
-          href: track["href"],
-          is_playable: track["is_playable"],
-          linked_from: track["linked_from"],
-          restrictions: track["restrictions"],
-          name: track["name"],
-          popularity: track["popularity"],
-          preview_url: track["preview_url"],
-          track_number: track["track_number"],
-          type: track["type"],
-          uri: track["uri"],
-          is_local: track["is_local"],
-          artists: track["artists"],
-          album: track["album"]
-        }
-      end)
+    case response.status_code do
+      200 ->
+        decoded_response = response
+          |> Map.get(:body)
+          |> Jason.decode!()
+
+        Enum.map(decoded_response["tracks"], fn track ->
+          %Track{
+            id: track["id"],
+            available_markets: track["available_markets"],
+            disc_number: track["disc_number"],
+            duration_ms: track["duration_ms"],
+            explicit: track["explicit"],
+            external_ids: track["external_ids"],
+            external_urls: track["external_urls"],
+            href: track["href"],
+            is_playable: track["is_playable"],
+            linked_from: track["linked_from"],
+            restrictions: track["restrictions"],
+            name: track["name"],
+            popularity: track["popularity"],
+            preview_url: track["preview_url"],
+            track_number: track["track_number"],
+            type: track["type"],
+            uri: track["uri"],
+            is_local: track["is_local"],
+            artists: track["artists"],
+            album: track["album"]
+          }
+        end)
+      _ ->
+        {:error, %{"status" => response.status_code, "message" => response.body}}
     end
   end
 end
